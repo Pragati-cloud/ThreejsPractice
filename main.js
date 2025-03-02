048808eefd36ab41d33c201050bfb103decec00a
Pragati Mishra <144036589+Pragati-cloud@users.noreply.github.com>
2025-03-02 23:09:33 +0530

    Add files via upload
    Three.js 2nd Basic practice project.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+const canvas = document.getElementById( 'canvas');
 //create scene
 const scene = new THREE.Scene();
 scene.background = new THREE.Color('#F0F0F0');
@@ -10,33 +12,56 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.z = 5;
 
 //create and add cube
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.DodecahedronGeometry();
 const material = new THREE.MeshLambertMaterial({ color: '#468585' , emissive: '#468585'});
 const cube = new THREE.Mesh(geometry, material);
+
+const boxGeometry = new THREE.BoxGeometry(2, 0.1 ,2);
+const boxMaterial = new THREE.MeshLambertMaterial({ color: '#468585' , emissive: '#468585'});
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+box.position.y = -1.5;
+
 scene.add(cube);
+scene.add(box);
 
 
 //add light 
 
-const light = new THREE.DirectionalLight(0x9CDBA6, 10);
-light.position.set(0, 0, 1);
+const light = new THREE.SpotLight(0x9CDBA6, 100);
+light.position.set(1, 1, 1);
 scene.add(light);
 
 //setup renderer
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-//aminate the scene
+renderer.setPixelRatio(window.devicePixelRatio);
 
 
+//add orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
 
+//add animation
 function animate(){
   requestAnimationFrame(animate);
 
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
+  
+  box.rotation.y += 0.005;
+  controls.update();
   renderer.render(scene, camera);
 }
+
+//handle window resize
+window.addEventListener('resize', ()=>{
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+})
+
 animate();
